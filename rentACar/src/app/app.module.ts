43ced825be429c44bfb9cf694e86f640aae983f5
Serap Erkan <seraperkannn@gmail.com>
2023-10-05 19:10:46 +0300

    Add product component

--- rentACar/src/app/app.module.ts
+++ rentACar/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ProductComponent } from './components/product/product.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent,
+    ProductComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
