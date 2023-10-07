c7348fde6bf9aab2e0b2c1d395c8f1e099287371
Serap Erkan <seraperkannn@gmail.com>
2023-10-08 01:18:43 +0300

    Add to car

--- rentACar/src/app/components/car/car.component.ts
+++ rentACar/src/app/components/car/car.component.ts
@@ -1,59 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Car } from 'src/app/models/car';
 import { CarService } from 'src/app/services/car.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+  styleUrls: ['./car.component.css'],
 })
-export class CarComponent implements OnInit{
-  dataLoaded:boolean=false;
+export class CarComponent implements OnInit {
+  dataLoaded: boolean = false;
   cars: Car[] = [];
-  filterText:string="";
+  filterText: string = '';
   filteredCount = { count: 0 };
-  
-  constructor(private carService:CarService , private activatedRoute:ActivatedRoute){}
+
+  constructor(
+    private carService: CarService,
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService:CartService
+  ) {}
 
   ngOnInit(): void {
- 
-    this.activatedRoute.params.subscribe(params=>{
-      if(params["brandId"]){
-       setTimeout(() => {
-        this.getCarByBarand(params["brandId"]);
-        },1000); 
-      }
-      else{
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['brandId']) {
+        setTimeout(() => {
+          this.getCarByBarand(params['brandId']);
+        }, 1000);
+      } else {
         setTimeout(() => {
           this.getCar();
-          },2000); 
+        }, 2000);
       }
- 
-  })
-
+    });
   }
 
-getCar() {
-  this.carService.getAllCars().subscribe(response=>{
-    this.cars=response;
-    this.dataLoaded=true;
-  });
+  getCar() {
+    this.carService.getAllCars().subscribe((response) => {
+      this.cars = response;
+      this.dataLoaded = true;
+    });
+  }
+
+  getCarByBarand(brandId: number) {
+    this.carService.getCarByBrand(brandId).subscribe((response) => {
+      this.cars = response;
+      this.dataLoaded = true;
+    });
+  }
+
+  addToCar(car: Car) {
+    if(car.state==3){
+    this.toastrService.error("Araba Bakımda");
+    }
+    else{
+      this.cartService.addToCart(car);
+      this.toastrService.success("Sepet" +car.modelName);
+    }
+  }
 }
-
-getCarByBarand(brandId:number){
-  this.carService.getCarByBrand(brandId).subscribe(response=>{
-    this.cars=response;
-    this.dataLoaded=true;
-  })
-}
-
-addToCar(car:Car){
-  
-
-}
-
-
-}
-
-
