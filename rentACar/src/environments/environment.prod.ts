d04ef55b71122f4b76d4b6a78b20d7a1348c2f91
Serap Erkan <serap.erkan@etiya.com>
2023-10-12 21:37:05 +0300

    Update environment configuration for API endpoints

---
+++ rentACar/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const enviroment = {
+  production: true,
+  apiUrl: 'http://localhost:3000',
+
+}
---
+++ rentACar/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const enviroment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+}
